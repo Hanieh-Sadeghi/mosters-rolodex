fa59ccf05b1fc775982e1c0b7646286429f3b6a5
Hanieh-Sadeghi <haniehsadeghi2@outlook.com>
2023-11-03 16:21:56 +0330

    add componet search box

---
+++ src/componets/search-box/search-box-component.jsx
@@ -0,0 +1,18 @@
+import { Component } from "react";
+
+import "./search-box.styles.css";
+
+class SearchBox extends Component {
+  render() {
+    return (
+      <input
+        className={`search-box ${this.props.className}`}
+        type="search"
+        placeholder={this.props.placeholder}
+        onChange={this.props.onChangeHander}
+      />
+    );
+  }
+}
+
+export default SearchBox;
